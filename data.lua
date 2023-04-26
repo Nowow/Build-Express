f059b8c2a6a323a8bfb1af1d175fe833c64ca667
Nowow <animexones@gmail.com>
2023-04-26 16:28:54 +0300

    added dummy data stage stuff to data.lua

--- data.lua
+++ data.lua
@@ -1,2 +1,8 @@
 require("prototypes.train_stop")
 require("prototypes.buttons")
+
+--buex-ghost-on-water
+--load groups
+require("prototypes/groups")
+--load categories
+require("prototypes/categories")
