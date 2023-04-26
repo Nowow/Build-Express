c44d28ba51badb2179b42f37e35fc703bcc29219
Nowow <animexones@gmail.com>
2023-04-26 17:07:25 +0300

    more refactor in blueprints.lua, less dependency injection

--- lib/ghosts_on_water_port/blueprints.lua
+++ lib/ghosts_on_water_port/blueprints.lua
@@ -7,10 +7,21 @@ local util = require('util')
 local constants = require('constants')
 require('lib.ghosts_on_water_port.common')
 
+--function that replaced BlueprintEntity with respective dummy
+-- @tparam BlueprintEntity
+blueprints.ReplaceEntityWithDummy = function(entity)
+    if (dummyEntityPrototypeExists(entity.name)) then
+        --replace entity with dummy entity by renaming it, because it is merely BlueprintEntity
+        entity.name = constants.dummyPrefix .. entity.name
+    end
+    return entity
+end
+
+
 --- Update a single blueprint, applying the replacerFunction to every entity in the blueprint.
 -- @tparam LuaItemStack blueprint
 -- @tparam func replacerFunction
-blueprints.updateSingleBlueprint = function(blueprint, replacerFunction)
+blueprints.updateSingleBlueprint = function(blueprint)
     --Safety checks: make sure stack is a blueprint and valid
    if not blueprint then return end
    if not blueprint.valid_for_read then return end
@@ -23,27 +34,11 @@ blueprints.updateSingleBlueprint = function(blueprint, replacerFunction)
    if not blueprintEntities or # blueprintEntities == 0 then return end
 
    --replace blueprint entities with dummy entities using table.map
-   local dummyEntities = table.map(blueprintEntities, replacerFunction)
+   local dummyEntities = table.map(blueprintEntities, blueprints.ReplaceEntityWithDummy)
 
    --set the blueprint entities
    blueprint.set_blueprint_entities(dummyEntities)
 end
 
---- Update blueprints in a book, applying the replacerFunction to entities.
---
--- If the 'UpdateAllBlueprintsInBooks' runtime-global setting is enabled (which
--- is the default) then this updates all blueprints in the book, and recurs into
--- books inside. If not, it only updates the active blueprint in the book.
---
--- @tparam LuaItemStack stack
--- @tparam func replacerFunction
-
-blueprints.bpReplacerToDummy = function(entity)
-    if (dummyEntityPrototypeExists(entity.name)) then
-        --replace entity with dummy entity
-        entity.name = constants.dummyPrefix .. entity.name
-    end
-    return entity
-end
 
 return blueprints
