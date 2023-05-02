7850635bd1354326c44d36c69c3a7064d4b3fd20
Nowow <animexones@gmail.com>
2023-05-02 10:14:30 +0300

    added pathfinding proxy prototype

--- prototypes/proxy.lua
+++ prototypes/proxy.lua
@@ -4,4 +4,46 @@ local character = table.deepcopy(data.raw["character"]["character"])
 character.name = constants.spider_carrier_proxy_prototype_name
 character.collision_mask = {"ghost-layer"}
 
-data:extend({character})
+local pathing_collision_mask = {
+    "water-tile",
+    "colliding-with-tiles-only",
+    "not-colliding-with-itself"
+  }
+
+local pathing_proxy = {
+    name = constants.pathfinding_proxy_name,
+    collision_box = {{-0.5, -0.5}, {0.5, 0.5}},
+    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+    type = "simple-entity",
+    icon = "__core__/graphics/empty.png",
+    icon_size = 1,
+    icon_mipmaps = 0,
+    flags = {"placeable-neutral", "not-on-map"},
+    order = "z",
+    max_health = 1,
+    render_layer = "object",
+    collision_mask = pathing_collision_mask,
+    pictures = {
+      {
+        filename = "__core__/graphics/empty.png",
+        width = 1,
+        height = 1
+      }
+    }
+  }
+  
+local template_item = {
+    type = "item",
+    flags = {
+      "hidden"
+    },
+    name = constants.pathfinding_proxy_name,
+    place_result = constants.pathfinding_proxy_name,
+    icon = "__core__/graphics/empty.png",
+    icon_size = 1,
+    order = "z",
+    stack_size = 1
+}
+
+data:extend({character, pathing_proxy, template_item})
+
