288a9f4dcbdae58b8a2f0ac6079d659d3a25834c
Nowow <animexones@gmail.com>
2023-05-04 17:55:08 +0300

    fix font

--- prototypes/fonts.lua
+++ prototypes/fonts.lua
@@ -1,11 +1,11 @@
-require("constants")
+local constants = require("constants")
 
 local bluid_blueprint_hotkey_font ={
     type = "font",
-    name = constants.bluid_blueprint_hotkey_font,
+    name = constants.catch_blueprint_order_hotkey_font,
     from = "default-bold",
     spacing = '0.5',
-    size = 35
+    size = 30
 }
 
 data:extend({bluid_blueprint_hotkey_font})
